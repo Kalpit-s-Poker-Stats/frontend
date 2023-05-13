cd2333f1112976bcb7600f5eacca6fd950271f9f
kalpit56 <kalpitmody@gmail.com>
2023-05-13 12:59:09 -0500

    creating home-page and calling api

---
+++ poker-frontend/src/pages/home-page/home-page.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent {
+
+  url = 'http://147.135.113.14:5000/v1/account/hello';
+  name: string | undefined;
+  winnings: number | undefined;
+  response: Object | undefined;
+
+  constructor(private http: HttpClient) { }
+
+  getInfo() {
+    this.http.get(this.url).subscribe((res) => {
+      this.response = res;
+    })
+  }
+
+}
