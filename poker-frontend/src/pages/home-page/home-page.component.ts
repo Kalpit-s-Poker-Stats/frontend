ff6555bd8ab1e5025f1299d5b0a5e19375dd196f
b2patel99 <78934502+b2patel99@users.noreply.github.com>
2023-05-16 22:26:57 -0400

    feat(home-page): adding form and api call to put data into table
    * API Call
    
    Added OnInit function to make api calls to server
    
    * Reworked Forms
    
    Added forms and got submissions working locally.
    
    * Added API Post
    
    coded the onSubmit function and got it to successfully post the form data to the server
    
    * Basic HTML/CSS Styling
    
    addded some basic styling so we can input better and it looks nicer

--- poker-frontend/src/pages/home-page/home-page.component.ts
+++ poker-frontend/src/pages/home-page/home-page.component.ts
@@ -1,28 +1,53 @@
-import { Component } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClient } from '@angular/common/http';
-import { FormControl } from '@angular/forms';
+import { Component} from '@angular/core';
+import { HttpClient,  HttpHeaders } from '@angular/common/http';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+
+
+export class HomePageComponent{
 
   url = 'http://147.135.113.14:5000/v1/account/hello';
   name: string | undefined;
   winnings: number | undefined;
   response: Object | undefined;
 
-  form = new FormControl('');
+  sessionEntry = new FormGroup({
+    id: new FormControl(),
+    winnings: new FormControl(),
+    buy_in_amount: new FormControl(),
+    buy_out_amount: new FormControl(),
+    location: new FormControl(''),
+    date: new FormControl(''),
+  });
 
   constructor(private http: HttpClient) { }
+
 
   getInfo() {
     this.http.get(this.url).subscribe((res) => {
       this.response = res;
     })
+  }
+  onSubmit() {
+    const sessionForm = this.sessionEntry.value;
+    this.http.post('http://147.135.113.14:5000/v1/session/entry' +
+    '?id='+ sessionForm.id +
+    '&winnings=' + sessionForm.winnings +
+    '&buy_in_amount=' + sessionForm.buy_in_amount +
+    '&buy_out_amount=' + sessionForm.buy_out_amount +
+    '&location=' + sessionForm.location +
+    '&date=' + sessionForm.date , sessionForm)
+
+    .subscribe((res) => {
+
+    });
   }
 
 }
