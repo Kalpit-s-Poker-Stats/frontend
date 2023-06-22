375360780592ac37764df02b92b9c9cf1a487bc7
kalpit56 <kalpitmody@gmail.com>
2023-06-22 00:00:34 -0500

    feat(view-data): connecting and making api call

--- poker-frontend/src/pages/view-data-page/view-data-page.component.ts
+++ poker-frontend/src/pages/view-data-page/view-data-page.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-view-data-page',
@@ -11,12 +12,34 @@ export class ViewDataPageComponent {
   url = 'http://147.135.113.14:5000/v1/';
   response: any;
 
+  viewData = new FormGroup({
+    id: new FormControl(),
+    beg_date: new FormControl(),
+    end_date: new FormControl()
+  });
+
   constructor(private http: HttpClient) { }
 
   getInfo() {
-    this.http.get(this.url).subscribe((res) => {
+    const userInput = this.viewData.value;
+    const urlWithParams = 'session/user_data?id=' + userInput.id + '&beg_date=' + this.formatDateForApi(userInput.beg_date) + '&end_date=' + this.formatDateForApi(userInput.end_date);
+    console.log(urlWithParams);
+
+    this.http.get(this.url + urlWithParams)
+    .subscribe((res) => {
+      console.log(res);
       this.response = res;
     })
+  }
+
+  formatDateForApi(dateString: string): string {
+    const dateParts = dateString.split('-');
+    const year = dateParts[0];
+    const month = dateParts[1];
+    const day = dateParts[2];
+    
+    const formattedDate = `${month}%2F${day}%2F${year}`;
+    return formattedDate;
   }
 
 }
