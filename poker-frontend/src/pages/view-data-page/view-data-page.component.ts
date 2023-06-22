bdd53b3ccfcc612bc254831c18d42743ff92a6ed
kalpit56 <kalpitmody@gmail.com>
2023-06-22 00:22:03 -0500

    feat: adding form and api connection into view-data page (#9)
    * feat(session-entry-page): adding new page n routes
    
    * feat(view-data): adding view-data-page
    
    * feat(nav-bar): adding nav bar
    
    * feat(view-data): connecting and making api call
    
    * chore: removing console.logs

--- poker-frontend/src/pages/view-data-page/view-data-page.component.ts
+++ poker-frontend/src/pages/view-data-page/view-data-page.component.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Component, inject } from '@angular/core';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { map } from 'rxjs/operators';
@@ -14,14 +15,33 @@ export class ViewDataPageComponent {
   url = 'http://147.135.113.14:5000/v1/';
   response: any;
 
+  viewData = new FormGroup({
+    id: new FormControl(),
+    beg_date: new FormControl(),
+    end_date: new FormControl()
+  });
+
   constructor(private http: HttpClient) { }
 
   getInfo() {
-    this.http.get(this.url).subscribe((res) => {
+    const userInput = this.viewData.value;
+    const urlWithParams = 'session/user_data?id=' + userInput.id + '&beg_date=' + this.formatDateForApi(userInput.beg_date) + '&end_date=' + this.formatDateForApi(userInput.end_date);
+
+    this.http.get(this.url + urlWithParams)
+    .subscribe((res) => {
       this.response = res;
     })
   }
 
+  formatDateForApi(dateString: string): string {
+    const dateParts = dateString.split('-');
+    const year = dateParts[0];
+    const month = dateParts[1];
+    const day = dateParts[2];
+    
+    const formattedDate = `${month}%2F${day}%2F${year}`;
+    return formattedDate;
+  }
   private breakpointObserver = inject(BreakpointObserver);
 
   /** Based on the screen size, switch from standard to one column per row */
