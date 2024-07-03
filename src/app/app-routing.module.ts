5c715b994818b18653f7e1f5422c4008414c6d92
kalpit56 <kalpitmody@gmail.com>
2024-07-02 23:11:53 -0500

    fixing routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from 'src/pages/home-page/home-page.component';
 import { SessionEntryPageComponent } from 'src/pages/session-entry-page/session-entry-page.component';
+import { sessionEntryPageGuard } from 'src/pages/session-entry-page/session-entry-page.guard';
 import { ViewDataPageComponent } from 'src/pages/view-data-page/view-data-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'session-entry', component: SessionEntryPageComponent },
+  { path: 'session-entry', component: SessionEntryPageComponent, canActivate: [sessionEntryPageGuard]},
   { path: 'view-data', component: ViewDataPageComponent },
 ];
 
