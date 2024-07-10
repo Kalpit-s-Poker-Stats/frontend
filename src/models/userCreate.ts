5ab03ade4fba763bdc399e1bbd439bd02b30609c
kalpit56 <kalpitmody@gmail.com>
2024-07-09 23:25:02 -0500

    updating sign up page

---
+++ src/models/userCreate.ts
@@ -0,0 +1,7 @@
+export interface userCreate {
+    name: string,
+    pn_id: string,
+    splitwise_email: string,
+    discord_username: string,
+    acknowledgment: boolean
+}
--- src/pages/form-page/form-page/form-page.component.ts
+++ src/pages/form-page/form-page/form-page.component.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 import { SplitwiseService } from 'src/services/splitwise.service';
+import { userCreate } from 'src/models/userCreate';
 
 @Component({
   selector: 'app-form-page',
@@ -11,19 +13,24 @@ import { SplitwiseService } from 'src/services/splitwise.service';
 export class FormPageComponent {
   name: string | undefined;
   winnings: number | undefined;
-  response: Object | undefined;
+  response: string;
   added: string | undefined;
   myLink = 'https://www.splitwise.com/join/GJWczrgAMYF';
+  url = environment.apiUrl;
 
   sessionEntry = new FormGroup({
     name: new FormControl(),
     pn_id: new FormControl(),
     splitwise_email: new FormControl(),
     discord_username: new FormControl(),
-    acknowledgement: new FormControl(),
+    acknowledgment: new FormControl(),
   });
 
+  userModel: userCreate;
+
   inputForm: FormGroup;
+
+  responseCodeFromEndpoint: number = -1;
 
   constructor(private http: HttpClient, private fb: FormBuilder, private splitwiseService: SplitwiseService) { }
 
@@ -35,6 +42,40 @@ export class FormPageComponent {
   }
 
   onSubmit() {
-    
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    });
+
+    console.log(this.sessionEntry.value);
+
+    this.http.post(this.url + "profile/create_user_profile", this.sessionEntry.value, { headers }).subscribe(
+      (response) => {
+        this.responseCodeFromEndpoint = 200;
+        this.response = response.toString();
+      },
+      (error) => {
+        this.responseCodeFromEndpoint = error.status;
+        console.log(this.responseCodeFromEndpoint);
+      }
+    )
+  }
+
+
+  errorCode(): number {
+    if(this.responseCodeFromEndpoint === 404) {
+      return 404;
+    } else if(this.responseCodeFromEndpoint === -1) {
+      return 200;
+    } else if( this.responseCodeFromEndpoint === 409) {
+      return 409;
+    } else {
+      return 500;
+    }
+  }
+
+  backToForm() {
+    this.responseCodeFromEndpoint = -1;
+    this.sessionEntry.reset();
   }
 }
